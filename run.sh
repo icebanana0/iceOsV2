254aa6a1f46fd0e87fda1b1267289c677cf692e6
icebanana0 <isaac.song12345.r.r@gmail.com>
2024-03-10 23:41:56 -0700

    added port drivers still working on print string

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-nasm -f bin boot.asm -o build/boot.bin
-gcc -ffreestanding -c kernel.c -o build/kernel.o
-ld -o build/kernel.bin -Ttext 0x1000 build/kernel.o --oformat binary
+nasm -f bin bootloader/boot.asm -o build/boot.bin
+gcc -ffreestanding -c kernel/kernel.c -o build/kernel.o
+gcc -ffreestanding -c drivers/ports.c -o build/ports.o
+# gcc -ffreestanding -c drivers/screen.c -o build/screen.o
+ld -o build/kernel.bin -Ttext 0x1000 build/kernel.o build/ports.o --oformat binary
 cat build/boot.bin build/kernel.bin > build/iceOs.bin
 qemu-system-x86_64 -drive format=raw,file=build/iceOs.bin
