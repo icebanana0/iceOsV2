c619c5ec42701c1bbd645ba4f9bd60ec4d95fb3a
isaaconrock <isaaconrock@gmail.com>
2024-04-09 20:50:19 -0700

    added scrolling

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,45 @@
-#include <stdint.h>
 #include "../types.h"
-#define VGA 0xb8000
+
 #define VGA_COLS 80
 #define VGA_ROWS 25
+#define VGA ((uint16*)0xB8000)
 
 void printc(char c, uint8 color, int x, int y) {
-    volatile uint16* mem = (uint16*)VGA;
+    volatile uint16* mem = VGA;
     uint16 entry = (color << 8) | c;
     uint16 offset = y * VGA_COLS + x;
     mem[offset] = entry;
+}
+
+void scroll() {
+    volatile uint16* mem = VGA;
+    for (int row = 0; row < VGA_ROWS - 1; row++) {
+        for (int col = 0; col < VGA_COLS; col++) {
+            mem[row * VGA_COLS + col] = mem[(row + 1) * VGA_COLS + col];
+        }
+    }
+
+    uint16 blank = 0x00 | ' ';
+    for (int col = 0; col < VGA_COLS; col++) {
+        mem[(VGA_ROWS - 1) * VGA_COLS + col] = blank;
+    }
+}
+
+void printString(const char* string, uint8 color, int x, int y) {
+    int i = 0;
+    while (string[i] != '\0') {
+        if (x >= VGA_COLS) {
+            x = 0;
+            y++;
+        }
+        if (y >= VGA_ROWS) {
+            scroll();
+            y--;
+        }
+        printc(string[i], color, x, y);
+        i++;
+        x++;
+    }
 }
 
 int main() {
@@ -16,11 +47,6 @@ int main() {
         printc(' ', 0x00, i % VGA_COLS, i / VGA_COLS);
     }
 
-    printc('H', 0x0F, 0, 0);
-    printc('e', 0x0F, 1, 0);
-    printc('l', 0x0F, 2, 0);
-    printc('l', 0x0F, 3, 0);
-    printc('o', 0x0F, 4, 0);
-
+    printString("Hello, World", 0x0F, 0, 0);
     return 0;
 }
